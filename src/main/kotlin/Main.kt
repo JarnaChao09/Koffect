4ac2c8d01cf2da32449c14ef48dcfb194f20fcb2
JarnaChao09 <yunyang4928@gmail.com>
2024-02-28 22:56:59 -0500

    Initial start on REPL

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
-public fun main() {
-    println("Hello Koffect!")
+import kotlin.system.exitProcess
+
+public fun main(args: Array<String>) {
+    if (args.size > 1) {
+        println("Flags currently unsupported")
+        exitProcess(64)
+    } else if (args.size == 1) {
+        println("Running file currently unsupported")
+        exitProcess(64)
+    } else {
+        repl()
+    }
+}
+
+public fun repl() {
+    var i = 0
+    while (true) {
+        i++
+        print("[$i]>>> ")
+        readlnOrNull()?.takeIf {
+            it != ":q"
+        }?.let {
+            println(it)
+        } ?: break
+    }
 }
